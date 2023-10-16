3cbd2cdc8d299f7deae1db904d85f140ede7308b
Akhmad Salafudin <axmad386@gmail.com>
2023-10-16 17:06:33 +0700

    feat: demo dependent select

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,54 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useMemo, useState } from "react";
+import "@mantine/core/styles.css";
+import { Button, MantineProvider, Select } from "@mantine/core";
 
+const groupSelect: Record<string, { label: string; value: string }[]> = {
+  a: [
+    { label: "", value: "" },
+    { label: "A1", value: "a1" },
+    { label: "A2", value: "a2" },
+  ],
+
+  b: [
+    { label: "", value: "" },
+    { label: "B1", value: "b1" },
+    { label: "B2", value: "b2" },
+  ],
+};
 function App() {
-  const [count, setCount] = useState(0)
+  const [select1, setSelect1] = useState("a");
+  const [select2, setSelect2] = useState("");
+  const childrenData = useMemo(() => {
+    return groupSelect[select1];
+  }, [select1]);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <MantineProvider>
+      <form>
+        <Select
+          label="parent"
+          value={select1}
+          onChange={(e) => setSelect1(e || "")}
+          data={[
+            {
+              label: "A",
+              value: "a",
+            },
+            {
+              label: "B",
+              value: "b",
+            },
+          ]}
+        ></Select>
+        <Select
+          label="children"
+          value={select2}
+          onChange={(e) => setSelect2(e || "")}
+          data={childrenData}
+        ></Select>
+      </form>
+    </MantineProvider>
+  );
 }
 
-export default App
+export default App;
